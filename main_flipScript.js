e1d5d56edfdb0014b75567fbc2a3f5ad8f9b391a
AlexTol <atol514@gmail.com>
2016-09-02 11:00:02 -0700

    Alex's patch 2: Electric boogaloo
    added folders, changed html of mainpage

--- main_flipScript.js
+++ main_flipScript.js
@@ -1,1 +1,10 @@
-alert(document.getElementById("square 1").getElementsByTagName("img"));
+var squareNames = ["square 1", "square 2", "square 3", "sqaure 4", "square 5", "square 6", "square 7", "square 8", "square 9"];
+
+var subPagePool = ["subPages\\MantiAnime.html", "subPages\\MantiCoding.html", "subPages\\MantiFood.html", "subPages\\MantiGaming.html", "subPages\\MantiMovie.html", "subPages\\MantiNews.html", "subPages\\MantiPhilosophical.html", "subPages\\MantiScienceandTechnology.html", "subPages\\MantiCodingHelp.html"];
+
+var displayStack = {};
+
+
+
+
+//document.getElementById("square 1").setAttribute("href", "I changed the link so it could be more dynamic");
