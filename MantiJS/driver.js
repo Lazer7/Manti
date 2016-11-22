2faba1fd2d94296c9be654f05626ff88800e50bf
AlexTol <atol514@gmail.com>
2016-11-22 11:34:47 -0800

    mainpage

--- MantiJS/driver.js
+++ MantiJS/driver.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 
+
 app.set('view engine', 'pug');
 app.set('views','./../MantiPug');
 
-
+app.use(express.static('../../Manti'));
 app.get('/mainpage', function(req, res){
     console.log('success');
     res.render('mainPage');
